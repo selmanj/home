e9cbbd9b380b2a94a76e43955f5bd49d00310f5e
Joe Selman <jsselman@google.com>
2016-03-24 16:00:47 -0700

    Don't launch ssh-agent on OS X

--- bash/ssh-agent.sh
+++ bash/ssh-agent.sh
@@ -4,20 +4,20 @@
 SSH_ENV="${HOME}/.ssh/environment"
 
 function start_agent {
-     echo "Starting SSH agent..."
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-     chmod 600 "${SSH_ENV}"
-     . "${SSH_ENV}" > /dev/null
+    echo "Starting SSH agent..."
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    chmod 600 "${SSH_ENV}"
+    . "${SSH_ENV}" > /dev/null
 }
 
-# Source SSH settings, if applicable
-
-if [ -f "${SSH_ENV}" ]; then
-     . "${SSH_ENV}" > /dev/null
-     ps -ef | grep "${SSH_AGENT_PID}" | grep ssh-agent$ > /dev/null || {
-         start_agent;
-     }
-else
-     start_agent;
+if [[ "$(uname -s)" -ne "Darwin" ]]; then
+    # Source SSH settings, if applicable
+    if [ -f "${SSH_ENV}" ]; then
+        . "${SSH_ENV}" > /dev/null
+        ps -ef | grep "${SSH_AGENT_PID}" | grep ssh-agent$ > /dev/null || {
+        start_agent;
+    }
+    else
+        start_agent;
+    fi
 fi
-
