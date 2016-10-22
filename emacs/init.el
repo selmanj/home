506e0c23968b77f7ad8681cfd7dcfb184ece780b
Joe Selman <jsselman@google.com>
2016-10-22 10:42:56 -0700

    More configuration (mainly clojure) and bell fix

--- emacs/init.el
+++ emacs/init.el
@@ -10,6 +10,9 @@
 (sensible-defaults/use-all-settings)
 (sensible-defaults/use-all-keybindings)
 
+;; Fix that annoying bell
+(load-file "~/.emacs.d/rwd-bell.el")
+
 ;; Ido mode and recentf seem pretty sweet
 (ido-mode t)
 (setq ido-enable-flex-matching t)
@@ -17,3 +20,25 @@
 (recentf-mode t)
 (setq recentf-max-saved-items 200)
 (setq ido-use-virtual-buffers t)
+
+;; Configure melpa-stable (stay away from melpa, been in trouble too much from that!)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+;; Require clojure-goodies
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+;; TODO add clj-refactor
+(add-hook 'clojure-mode-hook #'subword-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(unless (package-installed-p 'rainbow-delimiters)
+  (package-install 'rainbow-delimiters))
+(unless (package-installed-p 'aggressive-indent)
+  (package-install 'aggressive-indent))
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
