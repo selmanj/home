1705deeac1b940a63c73dfc0c8b39b60f6822aef
Joe Selman <jsselman@google.com>
2016-10-22 10:00:11 -0700

    Add sensible-defaults.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,11 @@
+;; Dark-themes are best 
 (load-theme 'tango-dark)
 
+;; Set up mac rebindings as advised by http://david.rothlis.net/emacs/osx.html
 (setq mac-command-modifier 'meta)
 (global-set-key (kbd "M-`") 'other-frame)
+
+;; I find sensible defaults very sensible
+(load-file "~/.emacs.d/sensible-defaults.el/sensible-defaults.el")
+(sensible-defaults/use-all-settings)
+(sensible-defaults/use-all-keybindings)
