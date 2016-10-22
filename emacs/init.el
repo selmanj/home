629f380dd9183fa3578dc650992bf7df210c8265
Joe Selman <jsselman@google.com>
2016-10-22 10:18:09 -0700

    Use ido mode and recentf

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-;; Dark-themes are best 
+;; Dark-themes are best
 (load-theme 'tango-dark)
 
 ;; Set up mac rebindings as advised by http://david.rothlis.net/emacs/osx.html
@@ -9,3 +9,11 @@
 (load-file "~/.emacs.d/sensible-defaults.el/sensible-defaults.el")
 (sensible-defaults/use-all-settings)
 (sensible-defaults/use-all-keybindings)
+
+;; Ido mode and recentf seem pretty sweet
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+(require 'recentf)
+(recentf-mode t)
+(setq recentf-max-saved-items 200)
+(setq ido-use-virtual-buffers t)
