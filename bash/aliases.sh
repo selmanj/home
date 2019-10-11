626d5881ce684be8c7aff43744dc6fd99f2cf819
Joe Selman <selman.joe@gmail.com>
2019-10-10 21:11:01 -0700

    Wiping to a clean slate since I never use this stuff anyway

--- bash/aliases.sh
+++
@@ -1,6 +0,0 @@
-alias tn="tmux new -s"
-alias ta="tmux attach -t"
-
-alias ls="ls -FG --color"
-alias dimmer="xrandr --output DVI-D-0 --brightness 0.7 --output DP-4 --brightness 0.7"
-alias undimmer="xrandr --output DVI-D-0 --brightness 1.0 --output DP-4 --brightness 1.0"
